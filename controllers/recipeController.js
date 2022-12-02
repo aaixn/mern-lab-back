631323b562c8af3a2ad7989db6cc58c61304e4d0
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-02 13:34:51 -0600

    search by title works

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -20,7 +20,19 @@ router.get('/',async (req, res, next) => {
 
 // SHOW
 // GET api/jobs/5a7db6c74d55bc51bdf39793
-router.get('/:id', (req, res) => {});
+router.get('/:title',async (req, res, next) => {
+    try{
+        const title = req.params.title.replace('%20', ' ').replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())
+        const recipe = await Recipe.find({title: title})
+        if (recipe) {
+            res.json(recipe)
+        } else {
+            res.sendStatus(404)
+        }
+    } catch (err) {
+        next(err)
+    }
+});
 
 // CREATE
 // POST api/jobs
