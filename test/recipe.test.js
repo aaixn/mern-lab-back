96cf514f8279e5ad0a7d3053da398a8755931d4b
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-06 19:21:57 -0600

    Test completed on GET with ID

--- test/recipe.test.js
+++ test/recipe.test.js
@@ -11,3 +11,45 @@ describe('GET /api/recipes', () => {
         .expect(200, done)
     })
 })
+
+// describe('GET /api/recipes/:id', () => {
+//     it('should return an object of a specific ID and contains the right fields'), done => {
+//         api
+//             .get('/api/recipes/638fdfd593ed8591ff3fbd7b')
+//             .set('Accept', 'application/json')
+//             .end((err, res) => {
+//                 expect(res, body).to.include({
+//                     _id: "638fdfd593ed8591ff3fbd7b",
+//                     title: "Braised Beef Chilli",
+//                     cookTime: "45 minutes"
+//                 })
+//                 done()
+//             })
+//     }
+// })
+
+
+describe('GET /api/recipes/:id', () => {
+    let id
+    before(done => {
+        api
+        .get('/api/recipes')
+        .set('Accept', 'application/son')
+        .end((err, res) => {
+            id = res.body[0]._id
+            done()
+        })
+    }) 
+    
+    it('retrieve a recipe by it\'s ID with the correct fields', done => {
+        api
+        .get(`/api/recipes/${id}`)
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+            expect(res.body._id).to.equal(id)
+            expect(res.body.title).to.equal('Braised Beef Chilli')
+            expect(res.body.cookTime).to.be.a('string')
+            done()
+        })
+    })
+})
